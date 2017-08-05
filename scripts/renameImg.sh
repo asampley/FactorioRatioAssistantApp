1bd26cd79423b680b044c6bc8ea6c24d65d964f1
asampley <sampley@ualberta.ca>
2017-08-05 10:50:00 -0600

    Fixed rename script to work outside of the target folder

--- scripts/renameImg.sh
+++ scripts/renameImg.sh
@@ -1,1 +1,14 @@
-rename  's/-/ /g; s/.*/lc($&)/e; s/(\/|^| )./uc($&)/ge' $1/*
+if [[ $# < 1 ]]; then
+	echo "Usage: $0 <directory>"
+	exit 1
+fi
+
+for filePath in "$1"/*; do
+	base="$(basename "${filePath}")"
+	dir="$(dirname "${filePath}")"
+	new=$(echo "${base}" | sed -r 's/-/ /g; s/[^ ]*/\u&/g; s/([^.]*)\$/\L\1/')
+	if [[ ${base} != ${new} ]]; then
+		mv "${dir}/${base}" "${dir}/${new}"
+		echo "${dir}/${base} -> ${dir}/${new}"
+	fi
+done
