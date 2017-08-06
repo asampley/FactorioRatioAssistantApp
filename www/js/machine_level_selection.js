fbfee57b8ec7ddfb949539a6b8a3e277e8106c74
asampley <sampley@ualberta.ca>
2017-08-06 08:56:10 -0600

    Added some code to prepare for switching mods in machine level selection

--- www/js/machine_level_selection.js
+++ www/js/machine_level_selection.js
@@ -8,74 +8,88 @@ document.addEventListener(
 		var mcButtons = {};
 		var machineButtons = {};
 
-		app.factorioEnvironment.addMachineClassListener(
-			function(machineClass) {
-				var machineSelector = treeTemplate.cloneNode(true);
-				machineSelector.classList.remove('hidden');
-				mainElement.appendChild(machineSelector);
-
-				var machineIcon = machineSelector.getElementsByClassName('tree-node-machine-icon')[0];
-				machineIcon.onerror = function() {this.onerror = null; this.src = 'img/default.png'};
-				machineIcon.src = app.factorioEnvironment.itemImgPaths[machineClass.name(0)];
-				machineSelector.getElementsByClassName('tree-node-machine-name')[0].textContent = machineClass.className;
-				machineButtons[machineClass.className] = machineSelector;
-
+		document.addEventListener(
+			'modsunloaded',
+			function() {
 				var childElement = machineSelector.getElementsByClassName('tree-node-children')[0];
-				var childTemplate = document.getElementById('template-button-machine-level');
-
-				machineSelector.getElementsByClassName('tree-node-button')[0].onclick = function() {
-					childElement.classList.toggle('hidden');
-				}
-
-				mcButtons[machineClass.className] = [];
-
-				for (var i = 0; i < machineClass.names.length; ++i) {
-					var machineLevelButton = childTemplate.cloneNode(true);
-					machineLevelButton.classList.remove('hidden');
-
-					var machineLevelIcon = machineLevelButton.getElementsByClassName('tree-node-machine-icon')[0];
-					machineLevelIcon.onerror = function() {this.onerror = null; this.src = 'img/default.png'};
-					machineLevelIcon.src = app.factorioEnvironment.itemImgPaths[machineClass.name(i)];
-					machineLevelButton.getElementsByClassName('tree-node-machine-name')[0].textContent = machineClass.name(i);
-
-					mcButtons[machineClass.className].push(machineLevelButton);
-
-					function createButtonClick(className, level) {
-						return function() {
-							app.ratioSolver.setMachineLevel(className, level);
-							console.log("Set machine class " + className + " to " + level);
-						}
-					}
-
-					machineLevelButton.onclick = createButtonClick(machineClass.className, i);
-
-					childElement.appendChild(machineLevelButton);
-
-					if (app.ratioSolver.getMachineLevel(machineClass.className) == i) {
-						machineLevelButton.style.backgroundColor = "var(--color-button-selected)";
-					}
-				}
+				childElementEmpty = childElement.cloneNode(false);
+				childElement.parentNode.replaceChild(childElementEmpty, childElement);
 			}
 		);
 
-		app.ratioSolver.addMachineLevelListener(
-			function(machineClass, level) {
-				if (!(machineClass.className in mcButtons)) return;
-				
-				var machineButton = machineButtons[machineClass.className];
-				var classButtons = mcButtons[machineClass.className];
+		document.addEventListener(
+			'modsloaded',
+			function() {
+				app.factorioEnvironment.addMachineClassListener(
+					function(machineClass) {
+						var machineSelector = treeTemplate.cloneNode(true);
+						machineSelector.classList.remove('hidden');
+						mainElement.appendChild(machineSelector);
 
-				var machineIcon = machineButton.getElementsByClassName('tree-node-machine-icon')[0];
-				machineIcon.onerror = function() {this.onerror = null; this.src = 'img/default.png'};
-				machineIcon.src = app.factorioEnvironment.itemImgPaths[machineClass.name(level)];
+						var machineIcon = machineSelector.getElementsByClassName('tree-node-machine-icon')[0];
+						machineIcon.onerror = function() {this.onerror = null; this.src = 'img/default.png'};
+						machineIcon.src = app.factorioEnvironment.itemImgPaths[machineClass.name(0)];
+						machineSelector.getElementsByClassName('tree-node-machine-name')[0].textContent = machineClass.className;
+						machineButtons[machineClass.className] = machineSelector;
 
-				for (var i = 0; i < classButtons.length; ++i) {
-					classButtons[i].style.backgroundColor = "";
-				}
+						var childElement = machineSelector.getElementsByClassName('tree-node-children')[0];
+						var childTemplate = document.getElementById('template-button-machine-level');
 
-				if (classButtons.length > level) {
-					classButtons[level].style.backgroundColor = "var(--color-button-selected)";
-				}
+						machineSelector.getElementsByClassName('tree-node-button')[0].onclick = function() {
+							childElement.classList.toggle('hidden');
+						}
+
+						mcButtons[machineClass.className] = [];
+
+						for (var i = 0; i < machineClass.names.length; ++i) {
+							var machineLevelButton = childTemplate.cloneNode(true);
+							machineLevelButton.classList.remove('hidden');
+
+							var machineLevelIcon = machineLevelButton.getElementsByClassName('tree-node-machine-icon')[0];
+							machineLevelIcon.onerror = function() {this.onerror = null; this.src = 'img/default.png'};
+							machineLevelIcon.src = app.factorioEnvironment.itemImgPaths[machineClass.name(i)];
+							machineLevelButton.getElementsByClassName('tree-node-machine-name')[0].textContent = machineClass.name(i);
+
+							mcButtons[machineClass.className].push(machineLevelButton);
+
+							function createButtonClick(className, level) {
+								return function() {
+									app.ratioSolver.setMachineLevel(className, level);
+									console.log("Set machine class " + className + " to " + level);
+								}
+							}
+
+							machineLevelButton.onclick = createButtonClick(machineClass.className, i);
+
+							childElement.appendChild(machineLevelButton);
+
+							if (app.ratioSolver.getMachineLevel(machineClass.className) == i) {
+								machineLevelButton.style.backgroundColor = "var(--color-button-selected)";
+							}
+						}
+					}
+				);
+
+				app.ratioSolver.addMachineLevelListener(
+					function(machineClass, level) {
+						if (!(machineClass.className in mcButtons)) return;
+						
+						var machineButton = machineButtons[machineClass.className];
+						var classButtons = mcButtons[machineClass.className];
+
+						var machineIcon = machineButton.getElementsByClassName('tree-node-machine-icon')[0];
+						machineIcon.onerror = function() {this.onerror = null; this.src = 'img/default.png'};
+						machineIcon.src = app.factorioEnvironment.itemImgPaths[machineClass.name(level)];
+
+						for (var i = 0; i < classButtons.length; ++i) {
+							classButtons[i].style.backgroundColor = "";
+						}
+
+						if (classButtons.length > level) {
+							classButtons[level].style.backgroundColor = "var(--color-button-selected)";
+						}
+					}
+				);
 			}
 		);
 
