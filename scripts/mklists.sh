d4d934dc6d781b74387e43b57edd5ad7f87be21f
asampley <sampley@ualberta.ca>
2017-08-07 13:51:59 -0600

    Made script to generate lists for mod versions, machines, and recipes

--- scripts/mklists.sh
+++ scripts/mklists.sh
@@ -6,20 +6,37 @@ lsPlus() {
 }
 
 for modPath in "${baseDir}"/*; do
+	if [ ! -d "${modPath}" ]; then
+		continue;
+	fi
+	
+	echo "Mod: ${modPath}"
+
+	versionsList="${modPath}/versions.txt"
+	rm "${versionsList}"
+	touch "${versionsList}"
 	for path in "${modPath}"/*; do
 		version=$(basename "${path}")
-		if [ ${version} == "img" ]; then
+		if [ "${version}" == "img" ]; then
+			continue;
+		fi
+		if [ ! -d "${path}" ]; then
 			continue;
 		fi
 		echo "Version: ${version}"
+		echo "${version}" >> "${versionsList}"
 		recipeList="${path}/recipes.txt"
+		machineList="${path}/machines.txt"
 		rm "${recipeList}"
 		touch "${recipeList}"
+		rm "${machineList}"
+		touch "${machineList}"
 		
 		recipePath="${path}/recipes/"
 		for machinePath in "${recipePath}"/*; do
 			machine=$(basename "${machinePath}")
 			
+			echo "${machine}" >> "${machineList}"	
 			echo "# ${machine}"
 			echo "# ${machine}" >> "${recipeList}"
 			for recipePath in "${machinePath}"/*; do
