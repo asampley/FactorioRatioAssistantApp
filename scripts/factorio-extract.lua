3f9b05c4205bdd9c3a1a135fb97831936d1f2ca9
sampley <sampley@ualberta.ca>
2018-06-04 15:11:20 -0600

    Added items, fluids, and machines to extraction process

---
+++ scripts/factorio-extract.lua
@@ -0,0 +1,60 @@
+/silent-command
+write_list = function (file_name, list_to_write)
+	table.sort(list_to_write)
+	output = "[\r\n\t" .. table.concat(list_to_write, ",\r\n\t") .. "\r\n]\r\n"
+	game.write_file(file_name, output)
+end
+
+list_recipes = {}
+for a, b in pairs(game.recipe_prototypes) do
+	recipe = "{ name:\"" .. b.name .. "\", energy:" .. b.energy .. ", category:\"" .. b.category .. "\""
+	
+	list_products = {}
+	for c,d in pairs (b.products) do
+		if d.amount ~= nil then
+			table.insert(list_products, "{ " .. d.name .. ":" .. d.amount .. " }")
+		end
+	end
+	recipe = recipe .. ", products: [ " .. table.concat(list_products, ", ") .. " ]"
+
+	list_ingredients = {}
+	for x,y in pairs (b.ingredients) do
+		table.insert(list_ingredients, "{ " .. y.name .. ":" .. y.amount .. " }")
+	end
+	recipe = recipe .. ", ingredients: [ " .. table.concat(list_ingredients, ", ") .. " ] }"
+	table.insert(list_recipes, recipe)
+end
+write_list("recipes.txt", list_recipes)
+
+
+list_items = {}
+for a, b in pairs(game.item_prototypes) do
+	item = "{ name:\"" .. b.name .. "\" }"
+	table.insert(list_items, item)
+end
+write_list("items.txt", list_items)
+
+
+list_fluids = {}
+for a, b in pairs(game.fluid_prototypes) do
+	fluid = "{ name:\"" .. b.name .. "\" }"
+	table.insert(list_fluids, fluid)
+end
+write_list("fluids.txt", list_fluids)
+
+
+list_machines = {}
+for a, b in pairs(game.entity_prototypes) do
+	if b.crafting_categories then
+		machine = "{ name:\"" .. b.name .. "\""
+
+		list_categories = {}
+		for c,d in pairs(b.crafting_categories) do
+			table.insert(list_categories, c)
+		end
+		machine = machine .. ", crafting_categories: [ \"" .. table.concat(list_categories, "\", \"") .. "\" ] }"
+
+		table.insert(list_machines, machine)
+	end
+end
+write_list("machines.txt", list_machines)
