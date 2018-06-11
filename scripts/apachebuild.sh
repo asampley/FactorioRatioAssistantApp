6dacca39f974065f579ef12b89a2c919503044b9
sampley <sampley@ualberta.ca>
2018-06-11 09:23:38 -0600

    Added android to builds

--- scripts/apachebuild.sh
+++ scripts/apachebuild.sh
@@ -1,2 +1,4 @@
 cordova build browser &&
+cordova build android --release &&
 sudo rsync -r platforms/browser/www/ /var/www/factorio/
+sudo cp platforms/android/app/build/outputs/apk/release/app-release.apk /var/www/factorio/build/android/factorio-ratio-assistant.apk
--- scripts/githubiobuild.sh
+++ scripts/githubiobuild.sh
@@ -1,2 +1,4 @@
 cordova build browser &&
+cordova build android --release &&
 rsync -r platforms/browser/www/ ../asampley.github.io/factorio/
+sudo cp platforms/android/app/build/outputs/apk/release/app-release.apk ../asampley.github.io/factorio/build/android/factorio-ratio-assistant.apk
