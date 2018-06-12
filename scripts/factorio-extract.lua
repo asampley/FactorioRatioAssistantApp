ad78b9e81a2242ed60b75e1dc31aed5c40657396
sampley <sampley@ualberta.ca>
2018-06-12 15:51:51 -0600

    Added module extraction

--- scripts/factorio-extract.lua
+++ scripts/factorio-extract.lua
@@ -1,14 +1,15 @@
 /silent-command
 write_table = function (file_name, table_to_write)
-	output = '{\r\n\t' .. table.concat(table_to_write, ',\r\n\t') .. '\r\n}\r\n'
+	local output = '{\r\n\t' .. table.concat(table_to_write, ',\r\n\t') .. '\r\n}\r\n'
 	game.write_file(file_name, output)
 end
 
-list_recipes = {}
+
+local list_recipes = {}
 for a, b in pairs(game.recipe_prototypes) do
-	recipe = '"' .. b.name .. '": { "energy":' .. b.energy .. ', "category":"' .. b.category .. '"'
+	local recipe = '"' .. b.name .. '": { "energy":' .. b.energy .. ', "category":"' .. b.category .. '"'
 	
-	list_products = {}
+	local list_products = {}
 	for c,d in pairs (b.products) do
 		if d.amount ~= nil then
 			table.insert(list_products, '"' .. d.name .. '":' .. d.amount)
@@ -16,7 +17,7 @@ for a, b in pairs(game.recipe_prototypes) do
 	end
 	recipe = recipe .. ', "products": { ' .. table.concat(list_products, ', ') .. ' }'
 
-	list_ingredients = {}
+	local list_ingredients = {}
 	for x,y in pairs (b.ingredients) do
 		table.insert(list_ingredients, '"' .. y.name .. '":' .. y.amount)
 	end
@@ -26,15 +27,28 @@ end
 write_table('recipes.txt', list_recipes)
 
 
-list_items = {}
+local list_items = {}
+local list_modules = {}
 for a, b in pairs(game.item_prototypes) do
-	item = '"' .. b.name .. '": { }'
+	local item = '"' .. b.name .. '": { }'
 	table.insert(list_items, item)
+
+	if b.module_effects then
+		local module = '"' .. b.name .. '": { '
+		
+		local list_effects = {}
+		for x,y in pairs (b.module_effects) do
+			table.insert(list_effects, '"' .. x .. '":' .. y.bonus)
+		end
+		module = module .. '"module_effects": { ' .. table.concat(list_effects, ', ') .. ' } }'
+		table.insert(list_modules, module)
+	end
 end
 write_table('items.txt', list_items)
+write_table('modules.txt', list_modules)
 
 
-list_fluids = {}
+local list_fluids = {}
 for a, b in pairs(game.fluid_prototypes) do
 	fluid = '"' .. b.name .. '": { }'
 	table.insert(list_fluids, fluid)
@@ -42,7 +56,8 @@ end
 write_table('fluids.txt', list_fluids)
 
 
-list_machines = {}
+local list_machines = {}
+local list_belts = {}
 for a, b in pairs(game.entity_prototypes) do
 	if b.crafting_categories then
 		speed = b.crafting_speed
@@ -59,15 +74,11 @@ for a, b in pairs(game.entity_prototypes) do
 
 		table.insert(list_machines, machine)
 	end
-end
-write_table('machines.txt', list_machines)
 
-
-list_belts = {}
-for a, b in pairs(game.entity_prototypes) do
 	if b.belt_speed then
 		belt = '"' .. b.name .. '": { "speed":' .. b.belt_speed .. ' }'
 		table.insert(list_belts, belt)
 	end
 end
+write_table('machines.txt', list_machines)
 write_table('belts.txt', list_belts)
