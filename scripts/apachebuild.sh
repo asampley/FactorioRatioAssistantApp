b4e3795be506b9a84e9cdaaa4d82d773c1e9ebc5
sampley <sampley@ualberta.ca>
2018-06-13 13:22:38 -0600

    Made scripts remove old files for website transfer

--- scripts/apachebuild.sh
+++ scripts/apachebuild.sh
@@ -1,4 +1,6 @@
 cordova build browser &&
 cordova build android --release &&
-sudo rsync -r platforms/browser/www/ /var/www/factorio/
+
+sudo rsync -r --delete platforms/browser/www/ /var/www/factorio/
+sudo mkdir -p /var/www/factorio/build/android/
 sudo cp platforms/android/app/build/outputs/apk/release/app-release.apk /var/www/factorio/build/android/factorio-ratio-assistant.apk
--- scripts/githubiobuild.sh
+++ scripts/githubiobuild.sh
@@ -1,4 +1,6 @@
 cordova build browser &&
 cordova build android --release &&
-rsync -r platforms/browser/www/ ../asampley.github.io/factorio/
-sudo cp platforms/android/app/build/outputs/apk/release/app-release.apk ../asampley.github.io/factorio/build/android/factorio-ratio-assistant.apk
+
+rsync -r --delete platforms/browser/www/ ../asampley.github.io/factorio/
+mkdir -p ../asampley.github.io/factorio/build/android/
+cp platforms/android/app/build/outputs/apk/release/app-release.apk ../asampley.github.io/factorio/build/android/factorio-ratio-assistant.apk
