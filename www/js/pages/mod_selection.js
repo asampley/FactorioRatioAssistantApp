a2c80f63c10949e324527f98acf004411e85b398
sampley <sampley@ualberta.ca>
2019-03-23 23:23:52 -0600

    Changed images in new mod pack

--- www/js/pages/mod_selection.js
+++ www/js/pages/mod_selection.js
@@ -1,58 +1,65 @@
 pages.mod_selection = {
-	modButtons: {}
-}
+	modButtons: {},
+	mainElement: {},
+	treeTemplate: {},
 
-document.addEventListener(
-	'deviceready', 
-	function() {
-		var mainElement = document.getElementById('mod_selection');
-		var treeTemplate = document.getElementById('template-tree-node-mod');
+	list_mods: function(text) {
 		var rootTreeNode = null;
 
-		fileutil.readTextAppWWW('mods/mods.txt', function(text) {
-			var modNames = [];
-
-			var lines = text.split('\n');
-			for (var i = 0; i < lines.length; ++i) {
-				var modName = lines[i].trim();
-				if (modName.length != 0) {
-					modNames.push(modName);
-				}
+		var modNames = [];
+		
+		var lines = text.split('\n');
+		for (var i = 0; i < lines.length; ++i) {
+			var modName = lines[i].trim();
+			if (modName.length != 0) {
+				modNames.push(modName);
 			}
+		}
 
-			for (var i = 0; i < modNames.length; ++i) {
-				var modName = modNames[i];
+		for (var i = 0; i < modNames.length; ++i) {
+			var modName = modNames[i];
+			console.log('Loading mod_selection button for ' + modName);
 
-				var modSelector = treeTemplate.cloneNode(true);
-				modSelector.classList.remove('hidden');
-				mainElement.appendChild(modSelector);
+			var modSelector = pages.mod_selection.treeTemplate.cloneNode(true);
+			modSelector.classList.remove('hidden');
+			pages.mod_selection.mainElement.appendChild(modSelector);
 
-				var modIcon = modSelector.getElementsByClassName('tree-node-mod-icon')[0];
-				modIcon.onerror = function() {this.onerror = null; this.src = 'img/default.png'};
-				modSelector.getElementsByClassName('tree-node-mod-name')[0].textContent = modName;
-				pages.mod_selection.modButtons[modName] = modSelector.getElementsByClassName('tree-node-button')[0];
+			var modIcon = modSelector.getElementsByClassName('tree-node-mod-icon')[0];
+			modIcon.onerror = function() {this.onerror = null; this.src = 'img/default.png'};
+			modSelector.getElementsByClassName('tree-node-mod-name')[0].textContent = modName;
+			pages.mod_selection.modButtons[modName] = modSelector.getElementsByClassName('tree-node-button')[0];
 
-				pages.mod_selection.modButtons[modName].onclick = function(mod) {
-					console.log('modname ' + mod);
-					return function() {
-						app.modLoader.mod = mod;
-						console.log('Set mod to "' + mod + '"');
-									
-						for (var modButtonName in pages.mod_selection.modButtons) {
-							pages.mod_selection.modButtons[modButtonName].style.backgroundColor = "";
-						}
-						this.style.backgroundColor = "var(--color-button-selected)";
+			pages.mod_selection.modButtons[modName].onclick = function(mod) {
+				console.log('modname ' + mod);
+				return function() {
+					app.modLoader.mod = mod;
+					console.log('Set mod to "' + mod + '"');
+								
+					for (var modButtonName in pages.mod_selection.modButtons) {
+						pages.mod_selection.modButtons[modButtonName].style.backgroundColor = "";
 					}
-				}(modName);
-
-				if (app.modLoader.mod == modName) {
-					pages.mod_selection.modButtons[modName].style.backgroundColor = "var(--color-button-selected)";
+					this.style.backgroundColor = "var(--color-button-selected)";
 				}
+			}(modName);
+
+			if (app.modLoader.mod == modName) {
+				pages.mod_selection.modButtons[modName].style.backgroundColor = "var(--color-button-selected)";
 			}
-		});
+			console.log('Added mod selector for mod with name "' + modName + '"');
+		}
+	}
+}
+document.addEventListener(
+	'fileutilready', 
+	function() {
+		pages.mod_selection.mainElement = document.getElementById('mod_selection');
+		pages.mod_selection.treeTemplate = document.getElementById('template-tree-node-mod');
+
+		fileutil.readTextAppWWW('mods/mods.txt', pages.mod_selection.list_mods);
+		//fileutil.readTextPersistent('mods/mods.txt', pages.mod_selection.list_mods);
 
 		content.addPage('mod_selection', new Page(
-			mainElement,
+			pages.mod_selection.mainElement,
 			undefined,
 			function() {
 				app.reloadMod();
